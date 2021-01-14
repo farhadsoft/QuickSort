2db24e6be5c3c04603d5cfcece0b0b9fb3b946d0
Farhad <epam@farhad.su>
2021-01-14 23:14:24 +0300

    All done!

--- QuickSort/Sorter.cs
+++ QuickSort/Sorter.cs
@@ -12,8 +12,7 @@ namespace QuickSort
         /// </summary>
         public static void QuickSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            RecursiveQuickSort(array);
         }
 
         /// <summary>
@@ -21,8 +20,72 @@ namespace QuickSort
         /// </summary>
         public static void RecursiveQuickSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            Sort(0, array.Length - 1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                {
+                    return;
+                }
+
+                int j = Partition(low, high);
+                Sort(low, j - 1);
+                Sort(j + 1, high);
+            }
+
+            int Partition(int low, int high)
+            {
+                int i = low;
+                int j = high + 1;
+
+                int pivot = array[low];
+                while (true)
+                {
+                    while (array[++i] < pivot)
+                    {
+                        if (i == high)
+                        {
+                            break;
+                        }
+                    }
+
+                    while (pivot < array[--j])
+                    {
+                        if (j == low)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (i >= j)
+                    {
+                        break;
+                    }
+
+                    Swap(array, i, j);
+                }
+
+                Swap(array, low, j);
+                return j;
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+            {
+                return;
+            }
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 }
